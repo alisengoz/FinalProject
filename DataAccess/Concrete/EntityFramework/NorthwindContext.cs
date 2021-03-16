b30776460231092258d6f71fdf86bb292e36d929
AliSengoz <77645221+alisengoz@users.noreply.github.com>
2021-03-16 22:26:33 +0200

    güncelleme

--- DataAccess/Concrete/EntityFramework/NorthwindContext.cs
+++ DataAccess/Concrete/EntityFramework/NorthwindContext.cs
@@ -12,7 +12,7 @@ namespace DataAccess.Concrete.EntityFramework
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=Northwind;Trusted_connection=true");
         }
-        public DbSet<Product> Product { get; set; }
+        public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet <Order> Orders { get; set; }
